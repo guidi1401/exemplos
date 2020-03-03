96b1148ece652b043b0068d98cb4e8077d348bd0
guidi1401 <amandaguidi2003@gmail.com>
2020-03-03 17:58:26 -0300

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head> 
+		<meta charset="UTF-8">
+		<title> Organizando Atividades</title>
+	</head>
+	<body>
+		<p> <h1> Página com links dos meus exemplos </h1> </p>
+		<hr />
+		<a href="20200303"> 20200303 - Exemplos </a>
+	</body>
+</html>
