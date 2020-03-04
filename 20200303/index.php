eb92b41ad304c3715e6ede8922f5671860177fc5
guidi1401 <amandaguidi2003@gmail.com>
2020-03-04 17:43:26 -0300

    exemplos

---
+++ 20200303/index.php
@@ -0,0 +1,20 @@
+!DOCTYPE html>
+<html lang="pt-BR">
+	<head> 
+		<meta charset="UTF-8">
+		<title>Organizando Exemplos</title>
+	</head>
+	<body>
+		<p> <h1> Página com links dos meus exemplos </h1> </p>
+		<hr />
+		<a href="20200303"> 20200303 - Exemplos </a>
+		<hr />
+		<a href="exemplo1.html"> Exemplo 1 </a>
+		<hr />
+		<a href="exemplo2.html"> Exemplo 2 </a>
+		<hr />
+		<a href="exemplo3.html"> Exemplo 3 </a>
+		<hr />
+		<a href="exemplo3a.html"> Exemplo 3a </a>
+	</body>
+</html>
